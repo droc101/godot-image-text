649339660ca78ab87a61e814b6aeccf5d50a20d4
droc101 <37421449+droc101@users.noreply.github.com>
2020-02-26 08:03:03 -0600

    Update TextRender.gd

--- TextRender.gd
+++ TextRender.gd
@@ -5,14 +5,17 @@ extends Control
 var counter = 0
 var vcounter = 0
 
-# Declare member variables here. Examples:
 export var text : String = "Test"
 export var position : Vector2 = Vector2(0, 0)
+
+
 export var letter_size : int = 16
-export var image_extension : String = ".png"
-export var letter_spacing : int = 0
+export var letter_spacing : int = 3
 export var vertical_spacing : int = 3
 
+export var image_extension : String = ".png"
+
+export var font_path : String = "res://font/"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -28,13 +31,13 @@ func _print(pos : Vector2, text : String):
 			counter = -1
 		if (i != " " and i != "/"):
 
-			var chart = TextureRect.new()
-			chart.texture = load("res://font/" + i.to_lower() + image_extension)
-			chart.rect_position.x = pos.x + counter*(letter_size + letter_spacing)
-			chart.rect_position.y = pos.y + vcounter*(letter_size + vertical_spacing)
-			chart.expand = true
-			chart.rect_size = Vector2(letter_size, letter_size)
-			add_child(chart)
+			var tex = TextureRect.new()
+			tex.texture = load(font_path + i.to_lower() + image_extension)
+			tex.rect_position.x = pos.x + counter*(letter_size + letter_spacing)
+			tex.rect_position.y = pos.y + vcounter*(letter_size + vertical_spacing)
+			tex.expand = true
+			tex.rect_size = Vector2(letter_size, letter_size)
+			add_child(tex)
 		counter += 1
 
 func _clear():
